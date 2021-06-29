459d96e5fda41bd9403b6ead1ee064ec828b3641
hemant-batra <hemant.batra@theinnovationinc.co>
2021-06-29 14:38:25 +0530

    encryption changes

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+N
+.idea/libraries/gson_2_2_2.xml,7\c\7c9eaae144b74ae75637386013bf1227595863d4
+B
+YPP_Encryption.iml,a\a\aaf312c16e49443aa5ee33fa7a65f6ea2fa7686c
--- src/Request_Encryption.java
+++ src/Request_Encryption.java
@@ -2,11 +2,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import javax.crypto.*;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +24,8 @@ public class Request_Encryption {
     static String json = "{\"body\": \"this is a sample\"}";
     static String partner_key = "sample_partner_key";
 
-    static String public_key_file_name = "kiteStore.p12";
-    static String public_key_file_password = "abc123";
-
-    static String private_key_file_name = "kiteStore.p12";
-    static String private_key_file_password = "abc123";
+    static String public_key_file_name = "public_key.pem";
+    static String private_key_file_name = "private_key.pem";
 
     public static void main(String[] args) throws Exception {
         System.out.println("Before encryption");
@@ -48,7 +52,7 @@ public class Request_Encryption {
         System.out.println("token = " + signedJson);
         System.out.println("partner = " + encryptedPartnerKey);
 
-        Map<String,String> requestBodyMap = new HashMap<>();
+        Map<String, String> requestBodyMap = new HashMap<>();
         requestBodyMap.put("body", encryptedJson);
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -87,10 +91,9 @@ public class Request_Encryption {
                and convert the output to Base64 Encoded String.
                # KEY #
      */
-    private static String encryptSymmetricKey(SecretKey secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException,
-            CertificateException, KeyStoreException, IOException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    private static String encryptSymmetricKey(SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey());
+        cipher.init(Cipher.ENCRYPT_MODE, readPublicKey());
         return Base64.getEncoder().encodeToString(cipher.doFinal(secretKey.getEncoded()));
     }
 
@@ -99,11 +102,10 @@ public class Request_Encryption {
                Supported algorithm is SHA1withRSA
                # TOKEN #
      */
-    private static String signConstructedJson(String encryptedJson) throws NoSuchAlgorithmException, UnrecoverableKeyException,
-            CertificateException, KeyStoreException, IOException, SignatureException, InvalidKeyException {
+    private static String signConstructedJson(String encryptedJson) throws Exception {
         byte[] data = Base64.getDecoder().decode(encryptedJson);
         Signature signature = Signature.getInstance("SHA1withRSA");
-        signature.initSign(getPrivateKey());
+        signature.initSign(readPrivateKey());
         signature.update(data);
         byte[] signatureBytes = signature.sign();
         return Base64.getEncoder().encodeToString(signatureBytes);
@@ -116,15 +118,14 @@ public class Request_Encryption {
                to Base64 Encoded String
                # PARTNER #
      */
-    private static String encryptPartnerKey() throws NoSuchPaddingException, NoSuchAlgorithmException, CertificateException,
-            KeyStoreException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+    private static String encryptPartnerKey() throws Exception {
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey());
+        cipher.init(Cipher.ENCRYPT_MODE, readPublicKey());
         byte[] encoded = cipher.doFinal(partner_key.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encoded);
     }
 
-    private static PublicKey getPublicKey() throws KeyStoreException, IOException,
+    /*private static PublicKey getPublicKey() throws KeyStoreException, IOException,
             CertificateException, NoSuchAlgorithmException {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(new FileInputStream(public_key_file_name), public_key_file_password.toCharArray());
@@ -137,5 +138,37 @@ public class Request_Encryption {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(new FileInputStream(private_key_file_name), private_key_file_password.toCharArray());
         return (PrivateKey) keyStore.getKey("kite-uat", private_key_file_password.toCharArray());
+    }*/
+
+    public static RSAPublicKey readPublicKey() throws Exception {
+        Path path = new File(public_key_file_name).toPath();
+        String key = new String(Files.readAllBytes(path), Charset.defaultCharset());
+
+        String publicKeyPEM = key
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replaceAll(System.lineSeparator(), "")
+                .replace("-----END PUBLIC KEY-----", "");
+
+        byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+        return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+    }
+
+    public static RSAPrivateKey readPrivateKey() throws Exception {
+        Path path = new File(private_key_file_name).toPath();
+        String key = new String(Files.readAllBytes(path), Charset.defaultCharset());
+
+        String privateKeyPEM = key
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replaceAll(System.lineSeparator(), "")
+                .replace("-----END PRIVATE KEY-----", "");
+
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
     }
 }
